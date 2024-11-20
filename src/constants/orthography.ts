43aae6b92356c1d1a32525bf8fc09e608c6791c1
Priyanka Samant <priyankasamant695@gmail.com>
2024-11-20 15:43:05 -0800

    Update orthography.ts

--- src/constants/orthography.ts
+++ src/constants/orthography.ts
@@ -1,55 +1,19 @@
 import { CONFIG } from './config'
 
 export const ORTHOGRAPHY = [
-  'a',
-  'aa',
-  'b',
-  'd',
-  'e',
-  'ee',
-  'g',
-  'g̱',
-  'gw',
-  'gy',
-  'h',
-  'hl',
-  'i',
-  'ii',
-  'j',
-  'k',
-  'ḵ',
-  "k'",
-  "ḵ'",
-  'kw',
-  "kw'",
-  'ky',
-  "ky'",
-  'l',
-  "'l",
-  'm',
-  "'m",
-  'n',
-  "'n",
-  'o',
-  'oo',
-  'p',
-  "p'",
-  's',
-  't',
-  "t'",
-  'ts',
-  "ts'",
-  'u',
-  'uu',
-  'w',
-  "'w",
-  'x',
-  'x̱',
-  'xw',
-  'y',
-  "'y",
-  "'",
-  '?',
+  'a'
+  'b'
+  'c'
+  'h'
+  'i'
+  't'
+  'r'
+  'm'
+  'n'
+  'z'
+  'r'
+  'k'
+  'u'
 ]
 
 if (CONFIG.normalization) {
